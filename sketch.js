d6c055d33ef7b50083a7dbab9d1c76c4246e5640
sweetypeajanelle-alt <sweetypeajanelle@gmail.com>
2026-05-03 00:49:48 -0700

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,70 @@
+let gifs = [];
+let sequence = [];
+let currentIndex = 0;
+let startTime = 0;
+let isWaiting = false;
+let delayTime = 1000; // 1 second
+let delayStart = 0;
+function preload() {
+	gifs = [{
+			img: loadImage('bear-heart.gif'),
+			duration: 7960
+		},
+		{
+			img: loadImage('bear-shy.gif'),
+			duration: 9560
+		},
+		{
+			img: loadImage('bear-walking.gif'),
+			duration: 6000
+		}
+	];
+}
+
+function setup() {
+	createCanvas(800, 800);
+	sequence = shuffle(gifs);
+	startGif();
+
+}
+
+function draw() {
+  background("white");
+
+  let currentGif = sequence[currentIndex];
+
+  imageMode(CENTER);
+
+  // Only draw gif if we're not in delay
+  if (!isWaiting) {
+    image(currentGif.img, width / 2, height / 2, 800, 800);
+
+    // check if gif finished
+    if (millis() - startTime > currentGif.duration) {
+      isWaiting = true;
+      delayStart = millis();
+    }
+  } else {
+    // during delay (screen stays blank or background only)
+
+    if (millis() - delayStart > delayTime) {
+      // move to next gif after delay
+      currentIndex++;
+
+      if (currentIndex >= sequence.length) {
+        sequence = shuffle(gifs);
+        currentIndex = 0;
+      }
+
+      startGif();
+      isWaiting = false;
+    }
+  }
+}
+
+function startGif() {
+	startTime = millis();
+
+	// restart gif from beginning
+	sequence[currentIndex].img.reset();
+}
